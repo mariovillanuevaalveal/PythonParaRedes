8d7d636679ce97b21a467ad8262b5f80c4748d9a
Mario Augusto Villanueva Alveal <mario.villanueva@inacapmail.cl>
2023-01-06 12:28:30 -0300

    Get Add Network from host Add

---
+++ GetNetIP.py
@@ -0,0 +1,8 @@
+# 
+# Este codigo obtiene la dirección de red a partir de una direccion de host 
+# esta ultima ingresada por teclado
+#
+import ipaddress
+
+host = ipaddress.ip_interface(input("Ingrese una dirección de host en el sigiente formato X.X.X.X/X "))
+print (f"La dirección de red del Host {host} es {host.network}  ")
