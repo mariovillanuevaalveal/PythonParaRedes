87dc4e50ee5c82dc6946262da5754b2d8bc9bae6
mariovillanuevaalveal <58556408+mariovillanuevaalveal@users.noreply.github.com>
2023-01-18 12:11:39 -0300

    Update ping.py
    Ejecución de prueba de ICMP echo-request

--- ping.py
+++ ping.py
@@ -1,3 +1,7 @@
+#
+# Ejecución de prueba de ICMP echo-request 
+# 
+#
 import os
 
 ip = "172.16.1.1"
@@ -6,4 +10,5 @@ echo_request = os.popen(f"ping -t 4 {ip} ").read()
 if ("icmp_seq=3") in echo_request: 
     print ("Full Ping")
 print (echo_request)
+
 print ("Fin")
