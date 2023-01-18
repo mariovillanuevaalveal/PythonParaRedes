172ae21ee6ae2b984b9d0fb2ba9aa1c685d4d887
mariovillanuevaalveal <58556408+mariovillanuevaalveal@users.noreply.github.com>
2023-01-18 12:18:18 -0300

    cambio de  nombre

--- ShowHostNetwork.py
+++ ShowHostNetwork.py
@@ -1,8 +1,11 @@
 # 
-# Este código obtiene las direcciones de host disponible a partir de una direccion de red.
+# Este código muestra las direcciones de host
+# a partir de una direccion de red.
 # esta última ingresada por teclado
 #
+
 import ipaddress
 
 host = ipaddress.ip_network(input("Ingrese una dirección de red en el sigiente formato X.X.X.X/X "))
+
 print (f"Esta red tiene disponible  {host.num_addresses-2} ")
