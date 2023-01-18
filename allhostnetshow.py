d0cb60329bf3ce4dba4b37ce4a4f598aad8be168
mariovillanuevaalveal <58556408+mariovillanuevaalveal@users.noreply.github.com>
2023-01-18 12:05:32 -0300

    Update and rename HostdeRed.py to allhostnetshow.py
    update

--- allhostnetshow.py
+++ allhostnetshow.py
@@ -1,6 +1,6 @@
 # Este codigo imprime todas las direcciones ip 
 # pertenecientes a una dirección de red dada por teclado. 
-#
+# Utilizacion de libreria ipaddress de python
 import ipaddress
 RedIp = ipaddress.ip_network(input("Ingrese una dirección de red en el sigiente formato X.X.X.X/X "))
 print (f"Los host que pertenesen a la red {RedIp} son > ")
